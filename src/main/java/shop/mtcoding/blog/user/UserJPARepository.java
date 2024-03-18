3f1e7f1b615bad50c25c56d97941778f35a6ae75
getinthere <getinthere@naver.com>
2024-03-18 12:39:05 +0900

    회원가입 서비스 구현완료

--- src/main/java/shop/mtcoding/blog/user/UserJPARepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserJPARepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 // 자동 컴퍼넌트 스캔이 된다.
 public interface UserJPARepository extends JpaRepository<User, Integer> {
 
     // findByUsernameAndPassword 로 작성하면 jpa query method가 query creation을 발동시킨다.
     // @Query("select u from User u where u.username = :username and u.password = :password")
-    User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+    Optional<User> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+    Optional<User> findByUsername(@Param("username") String username);
 }
